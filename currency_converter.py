733b584e1ba5f1b391b5a2de3e53e54cb085d558
Ashok Kumar <ashokkumar.ramajayam@my.jcu.edu.au>
2015-10-12 14:17:41 +1000

    cp1404

--- currency_converter.py
+++ currency_converter.py
@@ -1,35 +1,58 @@
-import web_utility;
-import CountryList;
+import web_utility
+from trip import Country
+
+cList = []
 
 def convert(amount, home_currency_code, location_currency_code):
-    url_string = "https://www.google.com/finance/converter?a="+str(amount)+"&from="+home_currency_code+"&to="+location_currency_code;
-    print("URL STRING: " + url_string);
+    url_string = "https://www.google.com/finance/converter?a="+str(amount)+"&from="+home_currency_code+"&to="+location_currency_code
     result = web_utility.load_page(url_string)
     resStr = result[result.index('result'):]
-    convtAmt = resStr[resStr.index("d>") + 2 :resStr.index(location_currency_code)];
-    return convtAmt;
+    convtAmt = resStr[resStr.index("d>") + 2 :resStr.index(location_currency_code)]
+    return float(convtAmt)
+
+
+def readFile():
+    file = open("currency_details.txt" ,encoding="utf8")
+    for line in file:
+        data = line.split(",")
+        c = Country(data[0], data[1], data[2].replace("\n",""))
+        cList.append(c)
+    file.close()
+
+def getDetails(countryName):
+    for country in cList:
+        if(country.name==countryName):
+            return country
+    return ""
 
 def main():
-    CountryList.readFile();
 
-    srcCtryName = input("Enter Source Country :");
-    srcCtry = CountryList.getDetails(srcCtryName);
-    while(srcCtry == ""):
-        print("Invalid Country. Try Again");
-        srcCtryName = input("Enter Source Country :");
-        srcCtry = CountryList.getDetails(srcCtryName);
+    readFile()
 
-    destCtryName = input("Enter Destination Country :");
-    destCtry = CountryList.getDetails(destCtryName);
-    while(destCtry == ""):
-        print("Invalid Country. Try Again");
-        destCtryName = input("Enter Destination Country :");
-        destCtry = CountryList.getDetails(destCtryName);
+    sourceCoutryName = input("Enter Home Country :")
+    sourceCountry = getDetails(sourceCoutryName)
+    if(sourceCountry == ""):
+        print("{:>10} {:>5} {:5}".format("Invalid Details ", sourceCoutryName, tuple()))
+    else:
+        print("{:>10} {:>5} {:5}".format("Valid Details ", sourceCoutryName, sourceCountry))
 
-    amount = int(input("Enter Amount:"));
-
-    print(convert(amount,srcCtry.code,destCtry.code));
+    destinationCountryName = input("Enter Trip Country :")
+    destinationCountry = getDetails(destinationCountryName)
+    if(destinationCountry == ""):
+        print("{:>10} {:>5} {:5}".format("Invalid Details ", destinationCountryName, tuple()))
+    else:
+        print("{:>10} {:>5} {:5}".format("Valid Details ", destinationCountryName, destinationCountry))
 
 
-main();
+    amount = int(input("Enter Amount:"))
+    if(sourceCountry == "" or destinationCountry == "" or sourceCountry.code == destinationCountry.code):
+        print("{:>10} {:>10} {:>10}->{:>2} {:>10} ".format("Invalid Conversion", float(amount),sourceCoutryName, destinationCountryName,"-1.00"))
+    else:
+        conversion = convert(amount, sourceCountry.code,destinationCountry.code)
+        reverseConversion = convert(conversion, destinationCountry.code, sourceCountry.code)
+        print("{:>10} {:10.2f} {:>10}->{:>2} {:10.2f} ".format("Valid Conversion  ", float(amount),sourceCountry.code, destinationCountry.code,conversion))
+        print("{:>10} {:10.2f} {:>10}->{:>2} {:10.2f} ".format("Reverse Conversion", conversion, destinationCountry.code, sourceCountry.code, reverseConversion))
+
+
+main()
 
--- trip.py
+++ trip.py
@@ -1,1 +1,48 @@
 __author__ = 'Ashok_kumar'
+import datetime
+
+class Country:
+    def __init__(self, name, code, symbol):
+        self.name = name
+        self.code = code
+        self.symbol = symbol
+
+    def __str__(self):
+        return "("+ self.name + ", " + self.code + ", " + self.symbol + ")"
+
+    def currency(self, amount):
+        return symbol + amount
+
+class Error(Exception):
+    def __init__(self, value):
+        Exception.value = value
+
+class Details:
+    locations = dict()
+
+    def __init__(self, locations):
+        self.locations = locations
+
+    def add(self, country_name, start_date, end_date):
+        try:
+            datetime.datetime.strptime(start_date, '%Y/%m/%d')
+            datetime.datetime.strptime(end_date, '%Y/%m/%d')
+            if(start_date >= end_date):
+                raise Error("Start Date should be smaller")
+            for location in locations:
+                date = location[1]
+                if(date[0] == start_date):
+                    raise Error("Start Date is already used in other Country")
+            locations[country_name] = tuple(start_date, end_date)
+        except:
+            raise Error("Incorrect data format, should be YYYY/MM/DD")
+
+    def is_empty(self):
+        return bool(locations)
+
+    def current_country(self, date_string):
+        for location in locations:
+            date = location[1]
+            if(date[0] < date_string < date[1]):
+                return location[0]
+        return Error("Date does not match with any available date in the list")
